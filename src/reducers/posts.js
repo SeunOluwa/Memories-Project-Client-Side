a92498f0f41bdbe9f9d5e06019e885571382f447
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-03-23 01:35:26 +0100

    second commit

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,5 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 
+// eslint-disable-next-line
 export default (posts = [], action) => {
     switch (action.type) {
         case DELETE:
